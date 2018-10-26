cb97473f95baf7563f19b9cd88ead96a82d2cf3f
chaudhary-anurag <anuragonservice@gmail.com>
2018-10-26 19:02:43 +0530

    Thread updation resolved

--- resources/assets/js/pages/Thread.vue
+++ resources/assets/js/pages/Thread.vue
@@ -8,14 +8,37 @@
        return {
          repliesCount:this.thread.replies_count,
          locked:this.thread.locked,
-         editing:false
+         editing:false,
+         title:this.thread.title,
+         body:this.thread.body,
+         form:{}
        };
+     },
+
+     created(){
+      this.resetForm();
      },
 
      methods: {
       toggleLock(){
-        axios[this.locked?'delete':'post']('/locked-threads/'+this.thread.slug);
+         let uri='/locked-threads/${this.thread.slug}';
+        axios[this.locked?'delete':'post'](uri);
         this.locked = !this.locked;
+      },
+      update(){
+        axios.patch('/threads/'+this.thread.channel.slug +'/' + this.thread.slug,this.form).then(()=>{
+          this.editing=false;
+          this.title=this.form.title;
+          this.body=this.form.body;
+          flash('Your thread has been updated.');
+        })
+      },
+      resetForm(){
+        this.form={
+          title:this.thread.title,
+          body:this.thread.body
+        };
+        this.editing=false;
       }
      }
   }
